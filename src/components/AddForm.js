3ad75a9d8de0aed51cd4cdf28e596006115ca1ad
Stephanie Zhu <slzzhu@gmail.com>
2020-09-05 22:27:32 -0700

    Added moment

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,30 +1,46 @@
 import React, { useState } from 'react';
 
 
-
-
 const AddForm = () => {
 
-  const [todos, setTodos] = useState('yo');
+  const [todos, setTodos] = useState(['hi', 'hello']);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // setTodos(event.target.value);
+    console.log(todos)
+
+  }
 
   const handleChange = (event) => {
-    // event.preventDefault();
     setTodos(event.target.value);
-    console.log(todos);
+    // let list = todos;
+    // setTodos(list.concat(event.target.value));
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+
+  }
+
+  let newTodo = todos
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, event.target.value]);
   }
 
   return (
     <>
-      <form onSubmit={handleChange}>
+      <form onSubmit={addTodo}>
         <input 
         type='text'
+        onChange={handleChange}
         placeholder='enter stuff'
         />
-        {/* <button type='submit'>add</button> */}
-        <input type='submit' value='Submit' />
+
+        <input type='submit' value='Add' />
       </form>
       <div>
-        {todos}
+      {/* {todos.map(item => <li>{item}</li>)} */}
+      {todos}
       </div>
     </>
   )
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,38 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
+import moment from 'moment';
 
 import AddForm from './AddForm.js';
 
+const weekdayshort = moment.weekdaysShort();
+const daysInMonth = moment().daysInMonth();
 
+let blanks = []
+
+for (let i = 0; i < daysInMonth; i++) {
+  blanks.push(<td>{i}</td>)
+}
+
+let firstDayOfMonth = () => {
+  // let firstDay = moment().startOf('month').format('d'); 
+  let firstDay = moment().startOf('month').format('d');
+ return firstDay;
+};
+
+
+let rows = [];
+let cells = [];
+
+blanks.forEach((row, i) => {
+  if (i % 7 !== 0) {
+    cells.push(row); // if index not equal 7 that means not go to next week
+  } else {
+    rows.push(cells); // when reach next week we contain all td in last week to rows 
+    cells = []; // empty container 
+    cells.push(row); // in current loop we still push current row to new container
+  }
+  if (i === blanks.length - 1) { // when end loop we add remain date
+    rows.push(cells);
+  }
+});
 
 const Calendar = () => {
+
+  const [selectDate, setSelectDate] = useState(moment().toDate())
   return (
     <div>
       <div>
         <AddForm />
-      </div>  
+      </div> 
+      <div>
+        {/* {moment().format('LL')} */}
+        {/* {[...Array(daysInMonth+1).keys()].map(item => <li>{item}</li>)} */}
+        {firstDayOfMonth()}
+      </div>
       <table>
+      
         <thead>
           <tr className='calendar'>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tues</th>
-            <th>Weds</th>
-            <th>Thurs</th>
-            <th>Fri</th>
-            <th>Sat</th>
-            <th>Sun</th>
+            {weekdayshort.map(day => <th>{day}</th>)}
           </tr>
         </thead>
         <tbody>
+      
           <tr className='topics'>
-            <td>1</td>
-            <td>Fried rice</td>
-            <td>2</td>
-            <td>4</td>
-            <td>3</td>
-            <td>2</td>
-            <td>1</td>
+            {blanks}
           </tr>
         </tbody>
       </table>
