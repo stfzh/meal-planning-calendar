0563d1d5edcfd949bf7706bf6b474d069df7bb3b
Stephanie Zhu <slzzhu@gmail.com>
2020-09-15 10:01:13 -0700

    Installed redux, added templates for redux

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import './App.css';
 // Components
 import Calendar from './components/Calendar.js';
 
+
 const App = () => {
   const [meals, setMeals] = useState([]);
+  const [costs, setCosts] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const db = firebase.firestore();
       const data = await db.collection('meals').get();
       setMeals(data.docs.map(i => ({...i.data(), id: i.id}))); 
+
+      const costsData = await db.collection('meals-costs').get();
+      setCosts(costsData.docs.map(d => ({...d.data(), id: d.id})));
     };
     fetchData();
   }, []);
@@ -24,8 +29,11 @@ const App = () => {
           <li key={meal.id}>{meal.cost} {meal.date} {meal.type} {meal.name} {meal.id}</li>
         ))}
       </ul>
+      <ul>
+        {costs.map(d => <li>{d.name} {d.cost} {d.id}</li>)}
+      </ul>
       <div>
-        <Calendar data={meals} />
+        <Calendar data={meals} costs={costs}/>
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -4,6 +4,44 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore} from 'redux';
+
+// STORE
+
+// ACTION
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER 
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+// display
+store.subscribe(() => console.log(store.getState()))
+
+// DISPATCH
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(decrement())
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
